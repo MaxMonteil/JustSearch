6df89a8dc616d1648ca2fd774c10e541fc702302
Maximilien Monteil <maximilienmonteil@gmail.com>
2020-07-14 16:10:48 +0300

    initial commit - clear boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,20 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <input type="text" v-model="query">
+    <SearchInput :query="query"/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import HelloWorld from './components/HelloWorld.vue'
+import SearchInput from './components/SearchInput.vue'
 
 @Component({
   components: {
-    HelloWorld
+    SearchInput
   }
 })
-export default class App extends Vue {}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+export default class App extends Vue {
+  public query = ''
 }
-</style>
+</script>
---
+++ src/components/SearchInput.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>JustSearch - {{ query }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class SearchInput extends Vue {
+  @Prop() private query!: string;
+}
+</script>
