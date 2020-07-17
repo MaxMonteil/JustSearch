fd8f09bcc244e2c8f0cfb3a380b7f9c9e6f3d91f
Maximilien Monteil <maximilienmonteil@gmail.com>
2020-07-17 09:40:30 +0300

    design: add a search svg icon to button

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -21,10 +21,14 @@
         @keyup.esc="clearSearch"
       >
       <button
-        class="px-4 py-1 text-lg font-medium text-white bg-blue-700 rounded-r-full focus:outline-none focus:bg-blue-800"
+        class="flex items-center px-4 py-1 text-lg font-medium text-white bg-blue-700 rounded-r-full focus:outline-none focus:bg-blue-800"
         type="submit"
       >
-        Search
+        <svg width="1em" height="1em" viewBox="0 0 16 16" class="inline-block mr-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
+          <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
+        </svg>
+        <span class="hidden sm:inline">Search</span>
       </button>
     </form>
   </div>
